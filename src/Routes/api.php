2fada0573507ba145703c081565831bd2a1e267a
Yiranubari <yiranubari4@gmail.com>
2026-04-15 13:48:45 +0100

    feat: add root health check route

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -2,8 +2,19 @@
 
 use App\Controllers\ProfileController;
 use Slim\App;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
+    // Health check route
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write(json_encode([
+            'status' => 'success',
+            'message' => 'Profile Intelligence API is running'
+        ]));
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
     $app->post('/api/profiles', [ProfileController::class, 'create']);
     $app->get('/api/profiles', [ProfileController::class, 'getAll']);
     $app->get('/api/profiles/{id}', [ProfileController::class, 'getOne']);
