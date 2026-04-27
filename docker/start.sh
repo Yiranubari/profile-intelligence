d1d15022a2854cabbc1b517d7c70b1934d3126a4
Yiranubari <yiranubari4@gmail.com>
2026-04-27 14:26:05 +0100

    feat(docker): streamline directory creation and permissions in Dockerfile and start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 php /var/www/html/database/seed.php
+
+# Make sure the seeded database is writable by php-fpm's user
+chown -R www-data:www-data /var/www/html/database
+chmod -R 775 /var/www/html/database
+
 php-fpm -D
 nginx -g 'daemon off;'
